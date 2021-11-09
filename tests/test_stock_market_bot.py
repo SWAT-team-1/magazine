d0f9d41c8ee98230a22a073a8cbde7c79ec805d0
Haneen Hashlamoun <haneen.hashlamoun@gmail.com>
2021-11-09 22:45:50 +0200

    test: stock market

--- tests/test_stock_market_bot.py
+++ tests/test_stock_market_bot.py
@@ -1,6 +1,7 @@
 from typing import Type
 import pytest
-from magazine.stock_market_bot import get_stock_table_report
+from magazine.stock_market_bot import get_stock_table_report, get_daily_stock_exchange
+import mock
 
 domain = 'https://www.ase.com.jo/en/bulletins/daily/new'
 test_df = get_stock_table_report(domain)
@@ -25,3 +26,29 @@ def test_get_stock_table_report_names_type():
 
 ##########################################################
 
+def test_first_stock_table_report():
+    with mock.patch('builtins.input', return_value="1"):
+        actual = get_daily_stock_exchange(test_df,'Haneen')
+        expected = '''****************************************************************************
+    ******                                                                ******
+    ******              Welcome to the STOCK MARKET section               ******
+    ******                                                                ******
+    ****************************************************************************
+    ******* MENU ==> Daily Stock Exchange (ASE) (1)
+    ******* MENU ==> Top Transactions (2)
+    ******* MENU ==> Search for Stock (3)
+    Bot: Please choose from the menu or q to quit
+    Haneen: '''
+    assert actual == expected
+
+def test_filtered_by_stock():
+    with mock.patch('builtins.input', return_value="3"):
+        expected = 'Bot: Pick the Stock symbol for more details (ex: ARBK) or q for quiting / * for menu'
+        actual = get_daily_stock_exchange(test_df,'Haneen')
+        assert actual == expected
+        
+def test_quitting_stock():
+    with mock.patch('builtins.input', return_value="q"):
+        expected = '************* THANKS FOR VISITING ...! *************'
+        actual = get_daily_stock_exchange(test_df,'Haneen')
+        assert actual == expected
